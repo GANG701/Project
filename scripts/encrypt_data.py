4b929d657df62f403cc5d6430d6ab7f3e331bfc4
GANG701 <dkabs65@gmail.com>
2025-08-17 02:52:09 +0900

    feat: encrypt_data

--- scripts/encrypt_data.py
+++ scripts/encrypt_data.py
@@ -71,7 +71,7 @@ try:
     conn = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='cjfdl2#mysql',
+        password='password',
         database='sea_system'
     )
     cur = conn.cursor()
